d720eb7c9b8f043890ca34790c5210e5fb165b0c
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2022-04-16 15:41:11 +0200

    Create 1920-build-array-from-permutation.py

---
+++ 1920-build-array-from-permutation.py
@@ -0,0 +1,14 @@
+# Link: https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+            
+        return ans
